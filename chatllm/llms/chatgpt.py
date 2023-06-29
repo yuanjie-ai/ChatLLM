fe0e7526b411daa1dbad6cca27ea906be28dfb86
Betterme <313303303@qq.com>
2023-06-29 09:26:01 +0800

    add: openai chat

--- chatllm/llms/chatgpt.py
+++ chatllm/llms/chatgpt.py
@@ -17,20 +17,20 @@ def load_llm4chat(**kwargs):
     openai.api_key = os.getenv("API_KEY", kwargs.get('api_key', 'chatllm'))
     openai.api_base = os.getenv("API_BASE", kwargs.get('api_base', "https://api.openai-proxy.com/v1"))
 
-    def stream_chat(query, history=None):
-        history = history or []  # [{"role": "system", "content": "你是东北证券大模型"}]
+    def stream_chat(query, history=None, **chat_kwargs):
+        history = history or []
         messages = history + [{"role": "user", "content": query}]
 
-        chat_kwargs = {
-            "model": "gpt-3.5-turbo-16k-0613",
+        kwargs = {
+            "model": "gpt-3.5-turbo-0613",
             "stream": True,
-            "max_tokens": 16000,
+            "max_tokens": 4000,
             "temperature": None,
             "top_p": None,
             "messages": messages,
             "user": "Betterme"
         }
-        completion = openai.ChatCompletion.create(**chat_kwargs)
+        completion = openai.ChatCompletion.create(**{**kwargs, **chat_kwargs})
 
         for c in completion:
             _ = c.choices[0].get('delta').get('content', '')
